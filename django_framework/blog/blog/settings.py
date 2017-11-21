f161ff813484be3fd571e8fedfae164fae9fbc28
Jan Gaura <jan.gaura@gmail.com>
2017-11-21 16:21:21 +0100

    Blog in Django part 2

--- django_framework/blog/blog/settings.py
+++ django_framework/blog/blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django_framework/blog/blog/urls.py
+++ django_framework/blog/blog/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import weblog.views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', weblog.views.index, name='index'),
+    url(r'^post/(?P<post_id>\d+)$', weblog.views.post, name='post'),
+    url(r'^post/(?P<post_id>\d+)/addcomment$', weblog.views.addcomment, name='addcomment'),
 ]
