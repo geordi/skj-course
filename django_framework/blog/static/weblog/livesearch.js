ec97837c1120aa1b3f911c42a2f4699be94b040f
Jan Gaura <jan.gaura@gmail.com>
2017-11-21 16:23:19 +0100

    Blog in Django part 3

---
+++ django_framework/blog/static/weblog/livesearch.js
@@ -0,0 +1,40 @@
+var xmlhttp;
+
+function showResult( str ) {
+    if ( str.length == 0 ) {
+        document.getElementById( "livesearch" ).innerHTML = "";
+        document.getElementById( "livesearch" ).style.border = "0px";
+        return;
+    }
+
+    xmlhttp = GetXmlHttpObject();
+    if ( xmlhttp == null ) {
+        alert( "Your browser does not support XML HTTP Request" );
+        return;
+    }
+    var url = "/livesearch";
+    url = url + "/" + str;
+    url = url + "/" + Math.random();
+    xmlhttp.onreadystatechange = stateChanged;
+    xmlhttp.open( "GET", url, true );
+    xmlhttp.send( null );
+}
+
+function stateChanged() {
+    if ( xmlhttp.readyState == 4 ) {
+        document.getElementById( "livesearch" ).innerHTML = xmlhttp.responseText;
+        document.getElementById( "livesearch" ).style.border = "1px solid #A5ACB2";
+    }
+}
+
+function GetXmlHttpObject() {
+    if ( window.XMLHttpRequest ) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        return new XMLHttpRequest();
+    }
+    if ( window.ActiveXObject ) {
+        // code for IE6, IE5
+        return new ActiveXObject( "Microsoft.XMLHTTP" );
+    }
+    return null;
+}
--- django_framework/blog/weblog/views.py
+++ django_framework/blog/weblog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 from .models import Post, Comment
 from .forms import CommentForm
@@ -29,3 +30,18 @@ def addcomment(request, post_id):
             comment.save()
 
             return redirect('post', post_id=post.id)
+
+def search(request):
+    return render(request, 'weblog/search.html', {})
+
+def livesearch(request, word, sid):
+    posts = Post.objects.filter(title__icontains=word)
+
+    if len(posts) > 0:
+        response = ''
+        for p in posts:
+            response += '<a href="' + reverse('post', args=(p.id,)) + '">{}</a><br/>'.format(p.title)
+    else:
+        response = 'no suggestion'
+
+    return HttpResponse(response)
