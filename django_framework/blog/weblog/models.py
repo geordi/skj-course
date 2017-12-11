cfeef671475860703533945c5f28c2fc43744e7c
duy9403 <33267089+duy9403@users.noreply.github.com>
2017-12-11 22:15:44 +0100

    Update models.py
    fix typo

--- django_framework/blog/weblog/models.py
+++ django_framework/blog/weblog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
-    pub_date = models.DateTimeField('date publiched', auto_now_add=True)
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
 
     def __str__(self):
         return self.title
