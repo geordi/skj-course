045b8f6426de0775eda4c1d2b382efeb1fe21e44
Jan Gaura <jan.gaura@gmail.com>
2021-02-15 16:05:51 +0100

    WIP: Ex 02

---
+++ labs/ex_02.py
@@ -0,0 +1,26 @@
+def envelope(string):
+    """
+    Returns input string with added leading and closing '!' character.
+    
+    Example:
+      "Hello" -> "!Hello!"
+    """
+    pass
+
+
+def use_envelope(lst):
+    """
+    Returns a list that contains strings that are enveloped using '!' character.
+    Use envelope function to decouple the problem.
+    """
+    pass
+
+
+def super_print(*arg):
+    """
+    Funkce bere libovolny pocet parametru jako print, ale parametr
+    muze byt jen retezec.
+    Vystup: vypise parametry stejne jako print, ale oddeli vystup
+    radou pomlcek, kterych je stejne jako bude vysledna delka retezce.
+    """
+    pass
